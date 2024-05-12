ee1f2f399e82393489ed46cd536318a5b1b580ba
Razvan1004 <razvan.florea@stud.ubbcluj.ro>
2024-05-12 22:19:33 +0200

    User Migrations

---
+++ Migrations/20240512160927_Users.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UBB_SE_2024_923_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    /*SongId = table.Column<int>(type: "int", nullable: false),
+                    ArtistName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genere = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubGenere = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),*/
+                    UserId = table.Column<string>(type: "int", nullable: false),
+                    Username = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Salt = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Age = table.Column<string>(type: "int", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users_UserId", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
