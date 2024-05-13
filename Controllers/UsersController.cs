00d7b599812bd4501ac1a1e5de8fb57e1db93c13
OCTAVIAN-MIHAIL GHEORGHE <octavian.gheorghe@stud.ubbcluj.ro>
2024-05-13 19:22:23 +0300

    Created a functional login/register function

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UBB_SE_2024_923_1.Models;
+using UBB_SE_2024_923_1.Repositories;
+
+namespace UBB_SE_2024_923_1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserRepository _repository;
+
+        public UsersController(UserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string newUserUsername, string newUserPassword, string newUserCountry, string newUserEmail, int newUserAge)
+        {
+            if (string.IsNullOrWhiteSpace(newUserUsername))
+            {
+                return BadRequest("Username is required");
+            }
+
+            if (string.IsNullOrWhiteSpace(newUserPassword))
+            {
+                return BadRequest("Password is required");
+            }
+
+            if (string.IsNullOrWhiteSpace(newUserCountry))
+            {
+                return BadRequest("Country is required");
+            }
+
+            if (string.IsNullOrWhiteSpace(newUserEmail))
+            {
+                return BadRequest("Email is required");
+            }
+
+            if (newUserAge < 0)
+            {
+                return BadRequest("Please select a valid age");
+            }
+
+            var user = new Users
+            {
+                UserName = newUserUsername,
+                Password = newUserPassword,
+                Country = newUserCountry,
+                Email = newUserEmail,
+                Role = 1
+            };
+
+            await _repository.BcryptPassword(user);
+            await _repository.Add(user);
+
+            return Ok(new { message = "Registration successful" });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string loginUsername, string loginPassword)
+        {
+            if (string.IsNullOrWhiteSpace(loginUsername))
+            {
+                return BadRequest("Username is required");
+            }
+
+            if (string.IsNullOrWhiteSpace(loginPassword))
+            {
+                return BadRequest("Password is required");
+            }
+
+            var user = await _repository.GetUserByUsername(loginUsername);
+
+            if (user == null)
+            {
+                return NotFound(new { message = "User not found" });
+            }
+
+            if (!_repository.VerifyPassword(loginPassword, user.Password))
+            {
+                return BadRequest(new { message = "Incorrect password" });
+            }
+
+            return Ok(new { message = "Login successful" });
+        }
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using UBB_SE_2024_923_1.Data;
+using UBB_SE_2024_923_1.Models;
+using BCrypt.Net;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.EntityFrameworkCore;
+
+namespace UBB_SE_2024_923_1.Repositories
+{
+    public class UserRepository : Repository<Users>, IUserRepository
+    {
+        public UserRepository(DataContext context) : base(context)
+        {
+        }
+
+        public async Task BcryptPassword(Users user)
+        {
+            string salt = "a$^#shfdyu$^%agb@#%jqd#!cbjhacs!@#!b";
+            string encryptedPassword = user.Password + salt;
+            user.Password = BCrypt.Net.BCrypt.HashPassword(encryptedPassword);
+            await _context.SaveChangesAsync();
+        }
+
+        public bool VerifyPassword(string inputPassword, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(inputPassword + "a$^#shfdyu$^%agb@#%jqd#!cbjhacs!@#!b", hashedPassword);
+        }
+
+        public async Task<Users> GetUserByUsername(string username)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == username);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return user;
+        }
+        public async Task EnableOrDisableAdmin(Users user)
+        {
+            if (user.Role == 1)
+            {
+                user.Role = 2;
+            }
+            else
+            {
+                user.Role = 1;
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
