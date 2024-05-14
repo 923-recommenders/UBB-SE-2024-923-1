b069abd2962066d2a77ff9a281b5b10e202520e0
913-Gânju-Alexandru <43420432+Gabjea@users.noreply.github.com>
2024-05-14 23:36:28 +0300

    Revert "Revert "auth-ui""

---
+++ UBB-SE-2024-923-1-UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UBB_SE_2024_923_1_UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
