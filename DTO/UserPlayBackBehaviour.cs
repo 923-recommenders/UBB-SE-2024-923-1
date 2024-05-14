8137c992467988bd60da5f4e3895e68b79febbc5
GeanovuMedeea <medeea.geanovu@gmail.com>
2024-05-14 23:24:43 +0300

    Added FullDetailsOnSongService and Tests

---
+++ DTO/UserPlayBackBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using UBB_SE_2024_923_1.Enums;
+
+namespace UBB_SE_2024_923_1.DTO
+{
+    public class UserPlayBackBehaviour
+    {
+        public int UserId { get; set; }
+
+        public int SongId { get; set; }
+
+        public PlaybackEventType EventType { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public UserPlayBackBehaviour()
+        {
+            UserId = 0;
+            SongId = 0;
+            EventType = PlaybackEventType.Like;
+            Timestamp = DateTime.Now;
+        }
+    }
+}
