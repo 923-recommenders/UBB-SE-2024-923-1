be2f2e0904c0d586c04b422707d71619545a2850
OCTAVIAN-MIHAIL GHEORGHE <octavian.gheorghe@stud.ubbcluj.ro>
2024-05-15 02:01:13 +0300

    hopeullly final change

---
+++ UBB-SE-2024-923-1/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace UBB_SE_2024_923_1.Services
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterUser(string username, string password, string country, string email, int age);
+        Task<string> AuthenticateUser(string username, string password);
+        Task<bool> EnableOrDisableArtist(int userId);
+    }
+}
